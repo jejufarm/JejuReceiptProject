d916fdf3f5aeff751edff6a6a56eec20ed15626d
cjw <cjw.git@gmail.com>
2022-01-01 08:47:44 +0900

    MainActivity Force Exit

--- app/src/main/java/kr/ac/koreatech/jejureceiptproject/view/activity/MainActivity.java
+++ app/src/main/java/kr/ac/koreatech/jejureceiptproject/view/activity/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.os.Bundle;
 import android.view.WindowManager;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
@@ -24,6 +25,10 @@ public class MainActivity extends AppCompatActivity {
         return imm;
     }
 
+    private long backKeyPressedTime = 0;
+    // 첫 번째 뒤로 가기 버튼을 누를 때 표시
+    private Toast toast;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,5 +44,24 @@ public class MainActivity extends AppCompatActivity {
                 1);
         CactusRecyclerViewModel.getInstance().getCacutList();
         setTitle("제주농원 영수증 앱");
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (System.currentTimeMillis() > backKeyPressedTime + 2500) {
+            backKeyPressedTime = System.currentTimeMillis();
+            toast = Toast.makeText(this, "뒤로 가기 버튼을 한 번 더 누르시면 종료됩니다.", Toast.LENGTH_LONG);
+            toast.show();
+            return;
+        }
+        // 마지막으로 뒤로 가기 버튼을 눌렀던 시간에 2.5초를 더해 현재 시간과 비교 후
+        // 마지막으로 뒤로 가기 버튼을 눌렀던 시간이 2.5초가 지나지 않았으면 종료
+        if (System.currentTimeMillis() <= backKeyPressedTime + 2500) {
+            android.os.Process.killProcess(android.os.Process.myPid());
+            toast.cancel();
+            toast = Toast.makeText(this, "이용해 주셔서 감사합니다.", Toast.LENGTH_LONG);
+            toast.show();
+
+        }
     }
 }
